9e7942c776145f2cde097feb3d5b4293ed01b7e1
Heidi Fryzell <heidi37@users.noreply.github.com>
2024-04-07 20:28:40 -0400

    initial commit

---
+++ data-types/exercise_1.py
@@ -0,0 +1,10 @@
+# 'True' - strings
+# False - boolean
+# (1, 2, 3) - tuples
+# 1.5 - floats
+# [1, 2, 3] - lists
+# 2 - integers
+# range(5) - ranges
+# {1, 2, 3} - sets
+# None - None
+# {'foo': 'bar'} - dictionary
