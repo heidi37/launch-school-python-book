e164041b615e4e6c0fad4229145b820b56580810
Heidi Fryzell <heidi37@users.noreply.github.com>
2024-04-07 21:05:57 -0400

    move exercises into correct folder

--- data-types/exercise_1.py
+++ data-types/exercise_1.py
@@ -1,10 +1,12 @@
-# 'True' - strings
-# False - boolean
-# (1, 2, 3) - tuples
-# 1.5 - floats
-# [1, 2, 3] - lists
-# 2 - integers
-# range(5) - ranges
-# {1, 2, 3} - sets
-# None - None
-# {'foo': 'bar'} - dictionary
+#Identify the data type or class for each of the following values:
+
+'True' # - string, str
+False # - boolean, bool
+(1, 2, 3) # - tuple, tuple
+1.5 # - float, float
+[1, 2, 3] # - list, list
+2 # - integer, int
+range(5) # - range, range
+{1, 2, 3} # - set, set
+None # - NoneType, NoneType
+{'foo': 'bar'} # - dictionary, dict
